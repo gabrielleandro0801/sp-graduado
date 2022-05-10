ef9259df7b24bfcb1ab2400475bc4d331e66d17c
Alexandre S. Rocha <alexandresouzarocha943@gmail.com>
2022-05-10 19:51:22 -0300

    Feature(frontend): Creating register type form

--- frontend/src/components/RegisterType.tsx
+++ frontend/src/components/RegisterType.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import StyledFormControlLabel from './styles/FormControlLabel';
+import CONSTANTS from '../commons/Constants';
 
 const RegisterType = (): JSX.Element => {
-  return <></>;
+  const [type, setType] = React.useState();
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setType((CONSTANTS.REGISTER_TYPE as any)[event.target.value]);
+  };
+
+  return (
+    <>
+      <FormControl>
+        <FormLabel id="controlled-register-type">Selecione o Tipo do seu Cadastro</FormLabel>
+        <RadioGroup
+          value={type}
+          onChange={handleOnChange}
+          aria-labelledby="contolled-register-type"
+          name="controlled-radio-button-register-type"
+        >
+          <StyledFormControlLabel value="GRADUATE" control={<Radio size="medium" />} label="Formando" />
+          <StyledFormControlLabel value="GODFATHER" control={<Radio size="medium" />} label="Padrinho" />
+        </RadioGroup>
+      </FormControl>
+    </>
+  );
 };
 
 export default RegisterType;
