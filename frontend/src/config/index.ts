989fdc495a9e84d19e77297ca7fe1f84f8458606
Alexandre S. Rocha <alexandresouzarocha943@gmail.com>
2022-05-20 22:59:46 -0300

    Feature(frontend): Fixing run scripts and code

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,8 +1,5 @@
 export default {
-  FACEBOOK: {
-    APP_CLIENT_ID: String(import.meta.env.VITE_FACEBOOK_APP_CLIENT_ID) || '1088597931155576',
-  },
-  GOOGLE: {
-    APP_CLIENT_ID: String(import.meta.env.VITE_GOOGLE_APP_CLIENT_ID) || '1088597931155576',
+  BACKEND: {
+    URL: import.meta.env.VITE_SP_GRADUADO_URL || 'NOT_PROVIDED',
   },
 };
